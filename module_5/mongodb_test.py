d55fb5f06bbae63403d7bab1ca11307bb8623e14
dhungelsanjiv <64886455+dhungelsanjiv@users.noreply.github.com>
2021-11-13 19:20:55 -0600

    created test file

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.aigyk.mongodb.net/pytech?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print("-- Pytech Collection List --")
+print(db.list_collection_names())
