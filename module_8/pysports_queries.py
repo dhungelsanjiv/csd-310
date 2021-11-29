a2da36731e72a2f4977909d3a4453e281e6b6ea1
dhungelsanjiv <64886455+dhungelsanjiv@users.noreply.github.com>
2021-11-28 22:58:36 -0600

    module 8

---
+++ module_8/pysports_queries.py
@@ -0,0 +1,55 @@
+""" import statements """
+import mysql.connector
+from mysql.connector import errorcode
+
+""" database config object """
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try:
+    """ try/catch block for handling potential MySQL database errors """ 
+
+    db = mysql.connector.connect(**config) # connect to the pysports database 
+    
+    # output the connection status 
+    print("\n  Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT team_id, team_name, mascot FROM team")
+
+    teams = cursor.fetchall()
+    print("-- DISPLAYING TEAM RECORDS --")
+    
+    for team in teams:
+        print("Team ID: {}".format(team[0]))
+        print("Team Name: {}".format(team[1]))
+        print("Mascot: {}".format(team[2]))
+        print("")
+
+    input("\n\n  Press any key to continue...")
+
+except mysql.connector.Error as err:
+    """ on error code """
+
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    """ close the connection to MySQL """
+
+    db.close()
+
+
+
