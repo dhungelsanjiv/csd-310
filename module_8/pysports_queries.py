338bbc202c7c3851a4da21ffddc044cf357f031b
dhungelsanjiv <64886455+dhungelsanjiv@users.noreply.github.com>
2021-11-29 19:19:06 -0600

    Module 8.3

--- module_8/pysports_queries.py
+++ module_8/pysports_queries.py
@@ -22,8 +22,10 @@ try:
     cursor = db.cursor()
 
     cursor.execute("SELECT team_id, team_name, mascot FROM team")
-
     teams = cursor.fetchall()
+
+    cursor.execute("SELECT player_id, first_name,last_name,team_id FROM player")
+    players = cursor.fetchall()
     print("-- DISPLAYING TEAM RECORDS --")
     
     for team in teams:
@@ -31,6 +33,19 @@ try:
         print("Team Name: {}".format(team[1]))
         print("Mascot: {}".format(team[2]))
         print("")
+
+    print("-- DISPLAYING PLAYER RECORDS --")
+
+    for player in players:
+        print("Player ID: {}".format(player[0]))
+        print("First Name: {}".format(player[1]))
+        print("Last Name: {}".format(player[2]))
+        print("Team ID: {}".format(player[3]))
+
+
+        print("")
+
+
 
     input("\n\n  Press any key to continue...")
 
